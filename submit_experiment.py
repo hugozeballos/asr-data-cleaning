a671f3e849c9b1fc50a771ceaeedd6f11e43606f
hugozeballos <Hzeballosmeneses@gmail.com>
2025-05-14 19:54:56 -0400

    large

--- submit_experiment.py
+++ submit_experiment.py
@@ -67,7 +67,7 @@ torchrun \\
   --nnodes=1 \\
   --nproc_per_node={gpus} \\
   --rdzv_backend=static \\
-  --rdzv_endpoint=localhost:12343 \\
+  --rdzv_endpoint=localhost:12344 \\
   main.py --config_path={copied_config_path}
 
 # Post-run cleanup
